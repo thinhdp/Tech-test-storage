aad667d4df54703dc6301810f2cc8fe8130b7513
thinhdp <thinhdp@users.noreply.github.com>
2023-08-23 11:35:04 +0800

    Delete NguyenQuangPhuc_interviewTest_updated/gic-test directory

--- NguyenQuangPhuc_interviewTest_updated/gic-test/README.md
+++
@@ -1,5 +0,0 @@
-# Getting Started by running `npm install`
-
-to install the dependencies and packages required for your the application
-
-# Note: This application is built for entrance examination, do not upload to github.
--- NguyenQuangPhuc_interviewTest_updated/gic-test/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import employeeReducer from "./employee-data/employeeSlice";
-
-export const store = configureStore({
-  reducer: {
-    employee: employeeReducer,
-  },
-});
