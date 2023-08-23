f743212ca603bc7b5addeb4b1f38b1821e55c631
thinhdp <thinhdp@users.noreply.github.com>
2023-08-23 11:34:50 +0800

    Delete LuongPhongBao_interviewTest_updated/interview directory

--- LuongPhongBao_interviewTest_updated/interview/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import memberReducer from '../features/members/memberSlice';
-
-export const store = configureStore({
-  reducer: {
-    members: memberReducer,
-  },
-});
