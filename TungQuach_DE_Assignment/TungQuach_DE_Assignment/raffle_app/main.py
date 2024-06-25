e483fe336f252f60e1a99fb12bcb1ea1746c4f43
thinhdp <thinhdp@users.noreply.github.com>
2024-06-25 14:07:31 +0800

    Delete TungQuach_DE_Assignment/TungQuach_DE_Assignment directory

--- TungQuach_DE_Assignment/TungQuach_DE_Assignment/raffle_app/main.py
+++
@@ -1,7 +0,0 @@
-from models.raffle_game import RaffleGame
-from modules.main.controller import MainScreenController
-
-raffle_game = RaffleGame()
-
-main_screen_controller = MainScreenController(raffle_game)
-main_screen_controller.run()
